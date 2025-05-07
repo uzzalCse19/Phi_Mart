d46909c1ad60a6e4d5bde7d2e10725c12e27e717
uzzalCse19 <uzzalhasan7878@gmail.com>
2025-05-07 09:29:29 +0600

    ProductImage Registerd Successfully

--- product/admin.py
+++ product/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from product.models import Product, Category, Review
+from product.models import Product, Category, Review,ProductImage
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Review)
+admin.site.register(ProductImage)
